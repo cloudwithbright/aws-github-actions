a6448f1a1b76353516ed4146a16f1e3678972893
Owusu Bright Debrah <odebrah-consultant@scholastic.com>
2024-01-13 09:34:10 +0000

    Testing to revise my memorry

--- app01/script.sh
+++ app01/script.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
-# Define variables
-Username="$1"
-Location="$2"
-Age="$3"
-
 # Display everything
-echo "My name is: $Username"
-echo "I stay at: $Location"
-echo "My age is: $Age"
+echo "My name is: $1"
+echo "My age is: $2"
+echo "I stay at: $3"
